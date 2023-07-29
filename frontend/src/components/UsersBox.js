55dfed952301af39ff971cd08653ccfb67fbc70d
Ezequiel Alarcon <138638611+EzeAlarcon@users.noreply.github.com>
2023-07-29 16:50:05 -0500

    Delete frontend directory

--- frontend/src/components/UsersBox.js
+++
@@ -1,9 +0,0 @@
-export default function UserBox({ name, password, id }) {
-    return (
-        <article>
-            <h3>{name}</h3>
-            <p>{password}</p>
-            <p>{id}</p>
-        </article>
-    )
-}
